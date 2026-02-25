867cc2be7daf124755a0ee8d515ef4b28161ef8a
Dharma2611 <dgangwal265@gmail.com>
2026-02-26 01:12:47 +0530

    add anotation with cressOrigin

--- src/main/java/com/code/Globle/Education/api/StudentApi.java
+++ src/main/java/com/code/Globle/Education/api/StudentApi.java
@@ -6,12 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.code.Globle.Education.Dto.StudentDto;
 import com.code.Globle.Education.Exception.GlobleEduucationException;
@@ -22,7 +17,7 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping(value = "globle-edu")
 @Validated
-
+@CrossOrigin(origins = "http://localhost:3000")
 public class StudentApi {
 	
 	@Autowired
